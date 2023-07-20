3da6c1864a4651959f60bff8700cf74d90bd382a
Fabio Baser <f.baser@monday-consulting.com>
2023-07-20 11:39:04 +0200

    adds telescop-file-browser

--- .config/nvim/lua/plugins/themes.lua
+++ .config/nvim/lua/plugins/themes.lua
@@ -1,12 +1,26 @@
 return {
-	{
-		"catppuccin/nvim",
-		name = "catppuccin",
-		opts = {
-			integrations = {
-				fidget = true,
-				lsp_saga = true,
-			},
-		},
-	},
+    {
+        "catppuccin/nvim",
+        name = "catppuccin",
+        priority = 1000,
+        opts = {
+            integrations = {
+                treesitter = true,
+                cmp = true,
+                gitsigns = true,
+                fidget = true,
+                lsp_saga = true,
+                notify = true,
+                leap = true,
+                dropbar = {
+                    enabled = true,
+                    color_mode = true,
+                },
+                mason = true,
+                neotree = true,
+                lsp_trouble = true,
+                which_key = false,
+            },
+        },
+    },
 }
