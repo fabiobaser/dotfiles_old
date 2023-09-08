7d35144d048e2811914140c02f85603b6ba4d13c
Fabio Baser <fabio.baser@monday-consulting.com>
2023-09-08 13:56:37 +0200

    improves Tab-For Snippets

--- .config/nvim/lua/config/cmp.lua
+++ .config/nvim/lua/config/cmp.lua
@@ -49,8 +49,12 @@ cmp.setup({
 		}),
 		["<CR>"] = cmp.mapping.confirm({ select = false }),
 		["<Tab>"] = vim.schedule_wrap(function(fallback)
-			if cmp.visible() and has_words_before() then
+			if luasnip.expand_or_jumpable() then
+				luasnip.expand_or_jump()
+			elseif cmp.visible() and has_words_before() then
 				cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+			elseif luasnip.expand_or_jumpable() then
+				luasnip.expand_or_jump()
 			else
 				fallback()
 			end
